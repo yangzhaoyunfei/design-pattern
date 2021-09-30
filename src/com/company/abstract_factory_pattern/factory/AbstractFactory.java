4810385e959b51e6d7ca8c4ab3fc425344e7e665
tang.zhong.wei <yangzhaoyunfei@qq.com>
2021-09-30 17:30:34 +0800

    更新工厂模式

--- src/com/company/abstract_factory_pattern/factory/AbstractFactory.java
+++ src/com/company/abstract_factory_pattern/factory/AbstractFactory.java
@@ -8,6 +8,12 @@ import com.company.abstract_factory_pattern.shape.Shape;
  * @date 2021/9/30 17:09
  */
 public abstract class AbstractFactory {
+	/**
+	 * 为 Color 和 Shape 对象创建抽象类来获取工厂。
+	 *
+	 * @param color
+	 * @return
+	 */
 	public abstract Color getColor(String color);
 
 	public abstract Shape getShape(String shape);
