8866aa402b1cdf3f1e96cf50906f5c5388cbfc5d
tang.zhong.wei <yangzhaoyunfei@qq.com>
2021-11-10 11:05:54 +0800

    提交人：yangzhaoyunfei 提交内容：享元模式 检视人：无

--- src/com/company/data_access_object/DaoPatternDemo.java
+++ src/com/company/data_access_object/DaoPatternDemo.java
@@ -1,5 +1,13 @@
 package com.company.data_access_object;
 
+/**
+ * 数据访问对象模式（Data Access Object Pattern）或 DAO 模式用于把低级的数据访问 API 或操作从高级的业务服务中分离出来。以下是数据访问对象模式的参与者。
+ * <p>
+ * 数据访问对象接口（Data Access Object Interface） - 该接口定义了在一个模型对象上要执行的标准操作。
+ * 数据访问对象实体类（Data Access Object concrete class） - 该类实现了上述的接口。该类负责从数据源获取数据，数据源可以是数据库，也可以是 xml，或者是其他的存储机制。
+ * 模型对象/数值对象（Model Object/Value Object） - 该对象是简单的 POJO，包含了 get/set 方法来存储通过使用 DAO 类检索到的数据。
+ * https://www.runoob.com/design-pattern/data-access-object-pattern.html
+ */
 public class DaoPatternDemo {
 	public static void main(String[] args) {
 		StudentDao studentDao = new StudentDaoImpl();
@@ -9,7 +17,6 @@ public class DaoPatternDemo {
 			System.out.println("Student: [RollNo : "
 					+ student.getRollNo() + ", Name : " + student.getName() + " ]");
 		}
-
 
 		//更新学生
 		Student student = studentDao.getAllStudents().get(0);
